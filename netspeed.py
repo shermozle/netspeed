f0d80dc44dd5a73bd562d13153f8fc0330ea0cee
Simon Rumble <simon@snowflake-analytics.com>
2018-04-23 16:24:58 +1000

    Put timestamp in with when it finished not started

--- netspeed.py
+++ netspeed.py
@@ -2,7 +2,6 @@
 
 import speedtest
 import datetime
-timestamp = datetime.datetime.utcnow()
 
 # If you want to test against a specific server
 servers = [2173]
@@ -13,7 +12,9 @@ try:
 	s.get_best_server()
 	s.download()
 	s.upload()
+	timestamp = datetime.datetime.utcnow()
 	results_dict = s.results.dict()
 	print(str(timestamp) + ',success,' + str(results_dict["download"]) + ',' + str(results_dict["upload"]) + ',' + str(results_dict["ping"]))
 except Exception, e:
+	timestamp = datetime.datetime.utcnow()
 	print(str(timestamp) + ',failure: ' + str(e))
